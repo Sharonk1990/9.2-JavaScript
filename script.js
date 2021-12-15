655188f4d318e0b81e4a452413f74f67ece47570
Sharonk1990 <92334366+Sharonk1990@users.noreply.github.com>
2021-12-15 20:36:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const age = 31 ;
+
+if (age >= 18){
+    console.log("Go inside! You are over 18");
+} else {
+    console.log("Please stay outside");
+}
+
+const isFemale = true;
+
+if (isFemale){
+    console.log("Womaaaan!");
+} else {
+    console.log("Get out!");
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    console.log("You are Bob!");
+}   else {
+    console.log("do not drive!");
+}
